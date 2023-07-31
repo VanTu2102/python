945131d7d69c92f76e03b279f1ec4eb1ae151c48
DESKTOP-KJNF2QE\Admin <vantu2102@gmail.com>
2023-07-31 17:32:57 +0700

    .

--- database/database.py
+++ database/database.py
@@ -1,14 +1,33 @@
 import psycopg2
 
-connection = psycopg2.connect(database="postgres", user="postgres", password="HoangVanTu2102@", host="localhost", port=5432)
 
-cursor = connection.cursor()
+class Postgres:
+    connection: any
+    cursor: any
+    database_name: str
+    user: str
+    password: str
+    host: str
+    port: int
 
-print(cursor)
+    def __init__(self, database_name, user, password, host, port) -> None:
+        self.database_name = database_name
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connection = psycopg2.connect(
+            database=database_name, user=user, password=password, host=host, port=port)
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
 
-# cursor.execute("SELECT * from account;")
+p = Postgres("postgres", 'hoangtu', 'HoangVanTu2102@', 'localhost', 5432)
 
-# # Fetch all rows from database
-# record = cursor.fetchall()
+# p.cursor.execute("CREATE TABLE IF NOT EXISTS your_table_name ( column1 integer, column2 bigint);INSERT INTO your_table_name VALUES (1,1092109);")
+p.cursor.execute("DROP TABLE your_table_name;")
+# p.cursor.execute("SELECT * from your_table_name;")
+
+# Fetch all rows from database
+# record = p.cursor.fetchall()
 
 # print("Data from Database:- ", record)
